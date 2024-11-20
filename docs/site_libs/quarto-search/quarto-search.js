f3a1832b4e546d7589967cbe99f94369ec194d5c
Yueer Liu <yueerl@uci.edu>
2024-11-19 22:59:07 -0500

    data

--- docs/site_libs/quarto-search/quarto-search.js
+++ docs/site_libs/quarto-search/quarto-search.js
@@ -1275,15 +1275,7 @@ async function fuseSearch(query, fuse, fuseOptions) {
 
   // If we don't have a subfuse and the query is long enough, go ahead
   // and create a subfuse to use for subsequent queries
-<<<<<<< HEAD
   if (now - then > kFuseMaxWait && subSearchFuse === undefined) {
-=======
-  if (
-    now - then > kFuseMaxWait &&
-    subSearchFuse === undefined &&
-    resultsRaw.length < fuseOptions.limit
-  ) {
->>>>>>> 58130d9b74aa92f149da51473959c72113f08532
     subSearchTerm = query;
     subSearchFuse = new window.Fuse([], kFuseIndexOptions);
     resultsRaw.forEach((rr) => {
